f39b61adcaa0fc5d21df34098f6987a5703481b9
thisisnotdevin <devin1299@outlook.com>
2023-04-03 23:00:19 -0400

    added menu

--- lyrical/src/components/Footer.js
+++ lyrical/src/components/Footer.js
@@ -4,13 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook,faTwitter,faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
+
 export default function Footer() {
   return (
     <div className="footer">
       <div className="footer-content">
         
         <ul className="social-media-links">
-          
+    
           <li><a href="#"><FontAwesomeIcon icon={faFacebook} /></a></li>
           <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
           <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
--- lyrical/src/components/MainPage.js
+++ lyrical/src/components/MainPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './MainPage.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 export default function MainPage() {
   return (
     
     <div className="main-page">
       <h1 className="main-page-heading">Feel the rhythm <br></br>Embrace the beat...
 </h1>
-      <button className="main-page-button">{'->'} Let's get started</button>
+      <button className="main-page-button"><FontAwesomeIcon icon={faArrowRight} /> Let's get started</button>
     </div>
   );
 }
