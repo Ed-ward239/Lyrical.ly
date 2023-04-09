7f806ddacdc2f1175c1e78fab369321f475c7580
Edward Lee <93949554+Ed-ward239@users.noreply.github.com>
2023-04-09 12:07:12 -0400

    updates

---
+++ lyrical/src/components/ParticlesBackground.js
@@ -0,0 +1,14 @@
+import Particles from "react-tsparticles"
+
+import React from "react"
+import particlesConfig from "./config/particles-config"
+
+const ParticlesBackground = () => {
+  return (
+    <Particles params={particlesConfig}>
+
+    </Particles>
+  )
+}
+
+export default ParticlesBackground
